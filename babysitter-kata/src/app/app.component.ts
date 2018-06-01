59ac005b2ad044c84cc9f3d31639d0eff5378cd6
Ben <benjamin.ng@careworks.com>
2018-06-01 14:00:21 -0400

    Set up the basic fields and loop for iterating through the hours

--- babysitter-kata/src/app/app.component.ts
+++ babysitter-kata/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Babysitter Kata';
+  timeMask = [/[0-1]/, /[0-9]/, ':', /[0-5]/, /[0-9]/, ' ', /[a|A|p|P]/, /[m|M]/];
+  startTime;
+  bedTime;
+  endTime;
+
+  doCalculation = () => {
+    const start = moment(this.startTime, 'h:m a');
+    const bed = moment(this.bedTime, 'h:m a');
+    const end = moment(this.endTime, 'h:m a');
+
+    let current = start;
+    
+    while(!(moment(current).format('h:m a') === moment(end).format('h:m a'))) {
+      current = moment(current).add(1, 'h');
+      console.log(current.format('h:m a'));
+    }
+  }
 }
--- babysitter-kata/src/app/app.module.ts
+++ babysitter-kata/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TextMaskModule } from 'angular2-text-mask';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TextMaskModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
